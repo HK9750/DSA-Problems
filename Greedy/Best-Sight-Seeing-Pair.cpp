c6b2bc92f3f1b77d2fb9a1d89fc9f492cf1e2827
HK9750 <hasnainkhan9750@gmail.com>
2024-12-27 22:59:54 +0500

    DCQ minor changes

--- Greedy/Best-Sight-Seeing-Pair.cpp
+++ Greedy/Best-Sight-Seeing-Pair.cpp
@@ -3,26 +3,26 @@
 class Solution
 {
 public:
-    int maxScoresSightseeingPair(vector<int> &values)
+    int maxScoreSightseeingPair(vector<int> &values)
     {
         int n = values.size();
         vector<int> left(n);
-        left[0] = values[0] + 0;
+        left[0] = values[0];
         int ans = 0;
 
         for (int i = 1; i < n; i++)
         {
-            int currLeft = values[i] + i;
-            left[i] = max(left[i - 1], curr);
-            int currRight = values[i] - i;
-            ans = max(ans, currLeft + currRight);
+            int curr = values[i] - i;
+            ans = max(ans, left[i - 1] + curr);
+            int right = values[i] + i;
+            left[i] = max(left[i - 1], right);
         }
 
         return ans;
     }
-}
+};
 
 // The problem states that we maximize (values[i] + i) + (values[j] - j) in which i < j.
 // So we keep count of maximum suffix array left which stores that maximum value of values[i] + i upto i-1 index.
-// Then in each iteration we calculate currLeft to update the maximum of that index and currRight to calculate the maximum answer.
+// Then we calculate the current value of values[i] - i and add it to the left[i-1] and store it in ans.
 // So in a greedy way we calculate the answer and in the end return it.
